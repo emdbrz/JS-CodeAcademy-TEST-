1e59011c0b13d7d8d705c8379e106793744dd304
emdbrz <126097576+emdbrz@users.noreply.github.com>
2023-04-28 22:12:18 +0300

    fift try

--- script.js
+++ script.js
@@ -1,26 +1,49 @@
-/* ------------------------------ TASK 1 ----------------------------
-Parašykite JS kodą, kuris leis vartotojui įvesti svorį kilogramais ir
-pamatyti jo pateikto svorio kovertavimą į:
-1. Svarus (lb) | Formulė: lb = kg * 2.2046
-2. Gramus (g) | Formulė: g = kg / 0.0010000
-3. Uncijos (oz) | Formulė: oz = kg * 35.274
+/* ------------------------------ TASK 3 -----------------------------------
+Parašykite JS kodą, kuris leis vartotojui paspaudus ant mygtuko "Show users"
+pamatyti vartotojus iš Github API (endpoint'as pateiktas žemiau).
 
-Pastaba: rezultatas turi būti matomas pateikus formą ir atvaizduojamas
-<div id="output"></div> viduje. Gautus atsakymus stilizuokite naudojant CSS;
-------------------------------------------------------------------- */
+Paspaudus mygtuką "Show users":
+1. Informacija atvaizduojama <div id="output"></div> bloke
+1.1. Informacija, kuri pateikiama: "login" ir "avatar_url" reikšmės (kortelėje)
+2. Žinutė "Press "Show Users" button to see users" turi išnykti;
 
+Pastaba: Sukurta kortelė, kurioje yra pateikiama vartotojo informacija, turi 
+būti stilizuota su CSS ir būti responsive;
+-------------------------------------------------------------------------- */
+
+const ENDPOINT = 'https://api.github.com/users';
+const button = document.getElementById('btn');
 const output = document.getElementById('output');
-const countKg = document.getElementById('submit-btn');
 
-countKg.addEventListener('click', function (e) {
-  e.preventDefault();
-  output.innerHTML = '';
-  const input = document.getElementById('search').value;
-  const pounds = input * 2.2046;
-  const grams = input / 0.001;
-  const ounce = input * 35.274;
+const userList = async () => {
+  try {
+    const response = await fetch(ENDPOINT);
+    const data = await response.json();
+    createUsers(data);
+  } catch (err) {
+    console.log(err);
+  }
+};
+button.addEventListener('click', () => {
+  button.setAttribute('disabled', true);
 
-  output.append(` Pounds: ${pounds}(lb),
-     Grams: ${grams}(g), 
-      Ounces: ${ounce}(oz)`);
+  userList();
 });
+const createUsers = (users) => {
+  output.innerHTML = '';
+  users.forEach((users) => {
+    const containerEl = document.createElement('div');
+    const imageEl = document.createElement('img');
+    const contentEl = document.createElement('div');
+    const loginEl = document.createElement('h2');
+
+    containerEl.classList.add('user');
+    imageEl.src = users.avatar_url;
+    imageEl.alt = 'user Image';
+    contentEl.classList.add('content');
+    loginEl.textContent = users.login;
+
+    containerEl.append(imageEl, loginEl);
+    output.append(containerEl);
+  });
+};
