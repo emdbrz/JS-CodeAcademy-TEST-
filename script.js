b2102040e93ad36d0e05e3ec8674f89798185036
emdbrz <126097576+emdbrz@users.noreply.github.com>
2023-05-02 15:55:01 +0300

    a tricky assignment with import and export

--- script.js
+++ script.js
@@ -1,26 +1,18 @@
-/* ------------------------------ TASK 1 ----------------------------
-Parašykite JS kodą, kuris leis vartotojui įvesti svorį kilogramais ir
-pamatyti jo pateikto svorio kovertavimą į:
-1. Svarus (lb) | Formulė: lb = kg * 2.2046
-2. Gramus (g) | Formulė: g = kg / 0.0010000
-3. Uncijos (oz) | Formulė: oz = kg * 35.274
+/* ------------------------------ TASK 10 ---------------------------------------------------
+Sutvarkykite užduoties "Task 10" esančius failus taip, kad veiktų žemiau pateiktos funkcijos.
+-------------------------------------------------------------------------------------------- */
+import { composition } from './modules/math/composition.js';
+import { division } from './modules/math/division.js';
+import { multiplication } from './modules/math/multiplication.js';
+import { substraction } from './modules/math/subtraction.js';
+import { one, two, three, four, five } from './modules/numbers/numbers.js';
 
-Pastaba: rezultatas turi būti matomas pateikus formą ir atvaizduojamas
-<div id="output"></div> viduje. Gautus atsakymus stilizuokite naudojant CSS;
-------------------------------------------------------------------- */
+let a = composition(one, four);
+let b = division(four, two);
+let c = substraction(three, two);
+let d = multiplication(five, two);
 
-const output = document.getElementById('output');
-const countKg = document.getElementById('submit-btn');
-
-countKg.addEventListener('click', function (e) {
-  e.preventDefault();
-  output.innerHTML = '';
-  const input = document.getElementById('search').value;
-  const pounds = input * 2.2046;
-  const grams = input / 0.001;
-  const ounce = input * 35.274;
-
-  output.append(` Pounds: ${pounds}(lb),
-     Grams: ${grams}(g), 
-      Ounces: ${ounce}(oz)`);
-});
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
